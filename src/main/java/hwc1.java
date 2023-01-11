77c10c21b3b50cb9ab1cddedd3fafff34c6fb67f
nitin nanda <nitnan00@in.ibm.com>
2023-01-11 22:21:52 +0530

    commit-hwc1

--- src/main/java/hwc1.java
+++ src/main/java/hwc1.java
@@ -1,4 +1,4 @@
 public class hwc1 {
     public static void main(String[] args) {
-        System.out.println("my kids ara real strong");    }
+        System.out.println("my kids ara head strong");    }
 }
